ca0620f5b958b4ab296f4852a4ca3390debe68af
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2024-11-26 17:11:07 +0000

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
 
+  get("/", {:controller: roll, :action : "homepage"})
+  get("/dice/2/6", {:controller: roll, :action : "flexible"})
+  get("/dice/2/10", {:controller: roll, :action : "flexible"})
+  get("/dice/1/20", {:controller: roll, :action : "flexible"})
+  get("/dice/5/4", {:controller: roll, :action : "flexible"})
+  get("/dice/:number_of_dice/:how_many_sides", {:controller: roll, :action : "flexible"})
+
 end
